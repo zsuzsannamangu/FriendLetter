65a93e26f47da7aaae3c3080706818980a87fcdf
zsuzsannamangu <zsuzsannamangu@gmail.com>
2019-04-27 21:27:30 -0700

    add route decorator

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -7,8 +7,11 @@ namespace FriendLetter.Controllers
   {
   //Hello() method represents a route in our application, sg a user can visit. because it is a route it will also create a special path. www.xyz.com/home/hello
   // /home, corresponds to the name of our controller
+  //add route decorator to override what URL path matches which route
+    [Route("/hello")]
     public string Hello() { return "Hello friend!"; }
 
+    [Route("/goodbye")]
     public string Goodbye() { return "Goodbye friend"; }
 
   }
